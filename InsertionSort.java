1a54af84dcea48d384322bb050d940fe784b119c
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-03-23 00:38:00 -0500

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,20 +6,20 @@ public class InsertionSort {
 		// Loop through the array
 		for (int i = 0; i < n-1; i++) {
 			int key = arr[i]; 
-            int j = i - 1; 
+            		int j = i - 1; 
   
-            while (j >= 0 && arr[j] > key) { 
-                arr[j + 1] = arr[j]; 
-                j = j - 1; 
-            } 
-            arr[j + 1] = key; 
-		}
+            		while (j >= 0 && arr[j] > key) { 
+                	arr[j + 1] = arr[j]; 
+                	j = j - 1; 
+            	} 
+            	arr[j + 1] = key; 
 	}
+}
 	
-	public static void main(String[] args) {
-		InsertionSort iSort = new InsertionSort();
-		int arr[] = { 12, 45, 2, 39, 77 };
-		iSort.sort(arr);
+public static void main(String[] args) {
+	InsertionSort iSort = new InsertionSort();
+	int arr[] = { 12, 45, 2, 39, 77 };
+	iSort.sort(arr);
 		
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
